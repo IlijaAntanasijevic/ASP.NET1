467e3d42373e9c06d110bf84bd98aabe490f8b18
Ilija <ilija0125@gmail.com>
2024-06-17 19:03:05 +0200

    fix: edit booking entity

--- Domain/Booking.cs
+++ Domain/Booking.cs
@@ -9,7 +9,7 @@ namespace App.Domain
     {
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
-        public Decimal TotalPrice { get; set; }
+        public Decimal? TotalPrice { get; set; }
         public int TotalGuests { get; set; }
         public int UserId { get; set; }
         public int ApartmentId { get; set; }
