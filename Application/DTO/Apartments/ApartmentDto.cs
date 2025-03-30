633b3522029e8cfa75207815439762354c94fee9
Ilija <ilija0125@gmail.com>
2025-03-30 18:24:52 +0200

    small changes

--- Application/DTO/Apartments/ApartmentDto.cs
+++ Application/DTO/Apartments/ApartmentDto.cs
@@ -7,25 +7,35 @@ using System.Threading.Tasks;
 
 namespace Application.DTO.Apartments
 {
-    public class SearchApartmentsDto
+    public class BaseApartmentDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int MaxGuests { get; set; }
         public decimal PricePerNight { get; set; }
         public string MainImage { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
         public string ApartmentType { get; set; }
     }
-
-    public class ApartmentDto : SearchApartmentsDto
+    public class SearchApartmentsDto : BaseApartmentDto
     {
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class ApartmentDto : BaseApartmentDto
+    {
+        public BasicDto City { get; set; }
+        public BasicDto Country { get; set; }
         public string Description { get; set; }
         public int TotalBookings { get; set; }
-        public IEnumerable<string> PaymentMethods { get; set; }
-        public IEnumerable<string> Features { get; set; }
+        public int ApartmentTypeId { get; set; }
+        public IEnumerable<BasicDto> PaymentMethods { get; set; }
+        //public IEnumerable<int> PaymentMethodIds { get; set; }
+        public IEnumerable<BasicDto> Features { get; set; }
+        //public IEnumerable<int> FeatureIds { get; set; }
         public IEnumerable<string> Images { get; set; }
+        public decimal Lattitude { get; set; }
+        public decimal Longitude { get; set; }
 
     }
 
